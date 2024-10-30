826cfdbe6e24f48d0ca9c7324167962f5191a84f
kimtoandev <kimvantoan2k3@gmail.com>
2024-10-30 20:50:27 +0700

    refactor:add error detection file authcontroller

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -8,6 +8,13 @@ export const register = async (req, res) => {
     return res.status(400).json({ message: "Hãy điền đầy đủ thông tin" });
   }
   const avatar = `https://avatar.iran.liara.run/username?username=${fullname}`;
+  const user = await User.findOne({ email });
+  if (user) {
+    return res.status(400).json({ errEmail: "Email đã được đăng kí" });
+  }
+  if (password.length < 6) {
+    return res.status(400).json({ errPassword: "Mật khẩu ít nhất 6 kí tự" });
+  }
   try {
     const hashedPassword = await bcrypt.hash(password, 10);
     const newUser = new User({
@@ -21,9 +28,8 @@ export const register = async (req, res) => {
   } catch (error) {
     res
       .status(400)
-      .json({ message: "Error registering user", error });
-      console.log(error);
-      
+      .json({ message: "Error registering user"});
+    console.log(error);
   }
 };
 
@@ -35,12 +41,12 @@ export const login = async (req, res) => {
   try {
     const user = await User.findOne({ email });
     if (!user) {
-      return res.status(400).json({ message: "Người dùng chưa được đăng kí" });
+      return res.status(400).json({ errEmail: "Email chưa được đăng kí" });
     }
 
     const isPasswordValid = await bcrypt.compare(password, user.password);
     if (!isPasswordValid) {
-      return res.status(401).json({ message: "Sai mật khẩu" });
+      return res.status(401).json({ errPassword: "Sai mật khẩu" });
     }
 
     const token = jwt.sign(
@@ -51,12 +57,17 @@ export const login = async (req, res) => {
       }
     );
 
-    res.cookie("token", token, { httpOnly: true, secure: true,expiresIn: "1d"});
-    res.status(200).json({ message: "Đăng nhập thành công" });
-  } catch (error) {
-    res.status(500).json({ message: "Error logging in", error});
-    console.log(error);
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: true,
+      expiresIn: "1d",
+    });
+    res.status(200).json({ user,message: "Đăng nhập thành công" });
+    console.log(user);
     
+  } catch (error) {
+    res.status(500).json({ message: "Error logging in", error });
+    console.log(error);
   }
 };
 
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import "dotenv/config";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
@@ -18,6 +19,12 @@ import connectCloudinary from "./configs/cloudinary.js";
 connectDB();
 connectCloudinary();
 const app = express();
+app.use(cors(
+  {
+    origin: "http://localhost:5173",
+    credentials: true
+  }
+));
 
 app.use(express.json());
 app.use(cookieParser());
